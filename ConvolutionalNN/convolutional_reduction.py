92b50ce75e5c2719779c6dc3df886e85426def07
ando-huang <ahuang09182000@gmail.com>
2021-01-18 17:39:39 -0500

    convolution simulator

---
+++ ConvolutionalNN/convolutional_reduction.py
@@ -0,0 +1,21 @@
+def printMatrix(matrix):
+    for i in range(len(matrix)):
+        print(matrix[i])
+
+def convolute(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    output = [[0 for col in range(cols-1)] for row in range(rows-1)]
+    convolute = [[2, 0], [0, 1]]
+    for i in range(len(output)):
+        for j in range(len(output[0])):
+            output[i][j] = matrix[i][j] * 1 + matrix[i+1][j+1] * 2 
+    return output
+
+testMatrix = [
+    [0, 10, 10, 0],
+    [20, 30, 30, 20],
+    [10, 20, 20, 10],
+    [0, 5, 5, 0]
+]
+printMatrix(convolute(testMatrix))
