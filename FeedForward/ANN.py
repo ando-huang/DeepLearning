0e10fc7bbf362c6dfca8f11073c0ff96a4809c98
ando-huang <ahuang09182000@gmail.com>
2021-01-18 17:07:23 -0500

    range updates

--- FeedForward/ANN.py
+++ FeedForward/ANN.py
@@ -5,7 +5,7 @@ import matplotlib as plt
 from mpl_toolkits.mplot3d import Axes3D
 
 N = 1000
-X = np.random.random((N, 2))*6-3
+X = np.random.random((N, 2)) * 10 - 5
 Y = np.cos(2*X[:,0]) + np.cos(3*X[:,1])
 
 fig = plt.figure()
